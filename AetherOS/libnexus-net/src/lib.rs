7a4ab0fb0af0e8a78bdb0c97c19a3ee9a819f01e
Aleksandar Kitipov <aeksandar.kitipov@gmail.com>
2026-04-14 13:41:38 +0300

    Refactor lib.rs by removing NetClient and NetError
    Removed NetClient struct and associated methods, along with NetError enum. Updated package metadata and dependencies.
    
    Signed-off-by: Aleksandar Kitipov <aeksandar.kitipov@gmail.com>

--- AetherOS/libnexus-net/src/lib.rs
+++ AetherOS/libnexus-net/src/lib.rs
@@ -1,36 +1,13 @@
-#![cfg_attr(not(feature = "std"), no_std)]
+[package]
+name = "libnexus-net"
+version = "0.1.0"
+edition = "2021"
 
-extern crate alloc;
-use alloc::vec::Vec;
+[dependencies]
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum NetError {
-    Unsupported,
-}
+[lib]
+path = "src/lib.rs"
 
-#[derive(Debug, Default)]
-pub struct NetClient;
-
-impl NetClient {
-    pub fn new() -> Self {
-        Self
-    }
-
-    pub fn open_udp_socket(&mut self, _port: u16) -> Result<u32, NetError> {
-        Ok(0)
-    }
-
-    pub fn send_to(
-        &self,
-        _socket_handle: u32,
-        _ip_address: [u8; 4],
-        _port: u16,
-        _payload: Vec<u8>,
-    ) -> Result<(), NetError> {
-        Ok(())
-    }
-
-    pub fn recv(&self, _socket_handle: u32) -> Result<Vec<u8>, NetError> {
-        Err(NetError::Unsupported)
-    }
-}
+[features]
+default = []
+std = []
